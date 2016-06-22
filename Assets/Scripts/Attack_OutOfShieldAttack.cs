e93218f0b7644d9f578097156153462736d38b17
Arne Urff <arne.urff@gmx.de>
2016-06-22 10:25:57 +0200

    Bug fixing Teil 2

--- Assets/Scripts/Attack_OutOfShieldAttack.cs
+++ Assets/Scripts/Attack_OutOfShieldAttack.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class Attack_OutOfShieldAttack : Attack {
 	
 	public void Attack(Rigidbody rigb, Animator anim, GameObject hurtBox) {
-		if (rigb.GetComponent<PlayerController>().GetCanMove()) {
+		if (rigb.GetComponent<PlayerController>().GetCanMove() || (!rigb.GetComponent<PlayerController>().GetCanMove() && rigb.GetComponent<PlayerController>().shield.activeSelf)) {
 			rigb.GetComponentInParent<PlayerController>().shield.SetActive(false);
 			rigb.GetComponentInParent<PlayerController>().HurtBoxTime(hurtBox, 1);
 			anim.Play("Block Attack");
 			rigb.GetComponent<PlayerController>().PlaySound(4);
+			rigb.GetComponent<PlayerController>().CantMoveFor(0.75f);
 		}
 	}
 }
--- Assets/Scripts/Movement_NormalBehaviour.cs
+++ Assets/Scripts/Movement_NormalBehaviour.cs
@@ -4,22 +4,25 @@ using System.Collections;
 public class Movement_NormalBehaviour : Movement {
 
 	public void Move(Rigidbody rigb, Animator anim, float horizontalForce, float moveForce, float maxSpeed) {
-		//Horizontal movement on the ground
-		if (Mathf.Abs (rigb.velocity.x) < maxSpeed) {
-			rigb.velocity = new Vector2(horizontalForce * moveForce, rigb.velocity.y);
-		}
-		else {
-			//Cutting the velocity over maxSpeed
-			//rigb.AddForce(Vector2.right * horizontalForce * moveForce);
-		}
 
-		if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") && rigb.velocity.x != 0) {
-			anim.CrossFade("init Dash", 0.01f);
-		}
+		if (GameObject.Find("CountdownText").GetComponent<TextMesh>().text == "") {
+			//Horizontal movement on the ground
+			if (Mathf.Abs (rigb.velocity.x) < maxSpeed) {
+				rigb.velocity = new Vector2(horizontalForce * moveForce, rigb.velocity.y);
+			}
+			else {
+				//Cutting the velocity over maxSpeed
+				//rigb.AddForce(Vector2.right * horizontalForce * moveForce);
+			}
 
-		if (rigb.GetComponent<PlayerController>().grounded && rigb.velocity.x == 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 01") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Block") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Block Attack")) {
-			//anim.CrossFade("Idle",0.1f);
-			anim.Play("Idle");
+			if (anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") && rigb.velocity.x != 0) {
+				anim.CrossFade("init Dash", 0.01f);
+			}
+
+			if (rigb.GetComponent<PlayerController>().grounded && rigb.velocity.x == 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Idle") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Attack 01") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Block") && !anim.GetCurrentAnimatorStateInfo(0).IsName("Block Attack")) {
+				//anim.CrossFade("Idle",0.1f);
+				anim.Play("Idle");
+			}
 		}
 	}
 }
